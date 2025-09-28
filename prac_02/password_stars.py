1975514a59f1beec2b77996a3b8d5c08ab58d64a
ZhangJiayu0201 <251537931@qq.com>
2025-09-28 13:54:07 +0800

    add password checking

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,6 +1,21 @@
 MINIMUM_LENGTH = 5
-password = input("Enter password: ")
-while len(password) < MINIMUM_LENGTH:
-    print(f"The length of your password must greater than {MINIMUM_LENGTH}!")
+
+
+def main():
+    password = get_password()
+    method_name(password)
+
+
+def method_name(password):
+    print("*" * len(password))
+
+
+def get_password():
     password = input("Enter password: ")
-print("*"*len(password))
+    while len(password) < MINIMUM_LENGTH:
+        print(f"The length of your password must greater than {MINIMUM_LENGTH}!")
+        password = input("Enter password: ")
+    return password
+
+
+main()
