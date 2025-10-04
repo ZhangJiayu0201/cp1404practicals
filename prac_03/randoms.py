38ec6ca2403ddb643eda2c8bcb3b4b43cd98efd2
ZhangJiayu0201 <251537931@qq.com>
2025-10-05 02:58:21 +0800

    complete reflection

---
+++ prac_03/randoms.py
@@ -0,0 +1,26 @@
+# What did you see on line 1?
+# What was the smallest number you could have seen, what was the largest?
+
+# A random number between 5 and 20 was produced.
+# The minimum number is 5, the maximum number is 20.
+
+
+# What did you see on line 2?
+# What was the smallest number you could have seen, what was the largest?
+# Could line 2 have produced a 4?
+
+# A random odd number between 3 and 10 was produced.
+# The minimum number was 3, and the maximum number was 9.
+# 4 could not be produced.
+
+
+# What did you see on line 3?
+# What was the smallest number you could have seen, what was the largest?
+
+# A random float number between 2.5 and 5.5 was produced.
+# The minimum number was 2.5, and the maximum number was 5.5.
+
+
+# Write code, not a comment, to produce a random number between 1 and 100 inclusive.
+import random
+print(random.randint(1, 100))
