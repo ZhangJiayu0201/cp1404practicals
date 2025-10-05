22f37e500a40fabb8397f9a397c05404824cbf98
ZhangJiayu0201 <251537931@qq.com>
2025-10-05 16:53:33 +0800

    complete all question

--- prac_03/file.py
+++ prac_03/file.py
@@ -4,17 +4,20 @@ name = input("Enter your name: ")
 print(name, file=infile)
 infile.close()
 
+
 # Q2.
 infile = open("name.txt", "r")
 name = infile.read()
 print(f"Hi {name.strip()}!")
 infile.close()
 
+
 # Q3.
 with open("number.txt", "r") as file:
     line1 = int(file.readline())
     line2 = int(file.readline())
     print(line1 + line2)
+
 
 # Q4.
 total = 0
