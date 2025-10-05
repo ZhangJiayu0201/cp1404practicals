59cc5c569b47b5a1c6e03f9250ce13e9b04f98f8
ZhangJiayu0201 <251537931@qq.com>
2025-10-05 17:34:15 +0800

    Add a WHILE loop stop condition and complete the loop

--- prac_03/exceptions_to_complete.py
+++ prac_03/exceptions_to_complete.py
@@ -11,4 +11,3 @@ while not is_finished:
     except ValueError:  # TODO - add the exception you want to catch after except
         print("Please enter a valid integer.")
 print("Valid result is:", result)
-
