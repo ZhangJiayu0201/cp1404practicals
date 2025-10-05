62c8de51d0d1b797e2dcab3b09ce1a0f480352d3
ZhangJiayu0201 <251537931@qq.com>
2025-10-05 16:44:21 +0800

    complete

---
+++ prac_03/file.py
@@ -0,0 +1,24 @@
+# Q1.
+infile = open("name.txt", "w")
+name = input("Enter your name: ")
+print(name, file=infile)
+infile.close()
+
+# Q2.
+infile = open("name.txt", "r")
+name = infile.read()
+print(f"Hi {name.strip()}!")
+infile.close()
+
+# Q3.
+with open("number.txt", "r") as file:
+    line1 = int(file.readline())
+    line2 = int(file.readline())
+    print(line1 + line2)
+
+# Q4.
+total = 0
+with open("number.txt", "r") as file:
+    for line in file:
+        total += int(line)
+print(total)
