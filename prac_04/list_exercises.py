b04f6ef304e74a54cb94db5e3db95440dc884d56
ZhangJiayu0201 <251537931@qq.com>
2025-10-12 20:21:07 +0800

    Commit get_numbers function and test

---
+++ prac_04/list_exercises.py
@@ -0,0 +1,19 @@
+def main():
+    numbers = []
+    get_numbers(numbers)
+    print(f"The first number is {numbers[0]}")
+    print(f"The last number is {numbers[-1]}")
+    print(f"The smallest number is {min(numbers)}")
+    print(f"The largest number is {max(numbers)}")
+    average = sum(numbers) / len(numbers)
+    print(f"The average of the numbers is {average}")
+
+
+def get_numbers(numbers):
+    for i in range(0, 5):
+        number = int(input("Number: "))
+        numbers.append(number)
+    return numbers
+
+
+main()
