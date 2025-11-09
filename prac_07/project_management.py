3a3a2bc6e4c091287744bf8fee2a4abc1c339d1b
ZhangJiayu0201 <251537931@qq.com>
2025-11-09 20:13:03 +0800

    Complete all the code & details of main function, and load & save function

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -4,7 +4,8 @@ Estimate time: 120 mins
 Actual time:   mins
 """
 
-
+import datetime
+from operator import itemgetter
 from project import Project
 
 
@@ -26,21 +27,50 @@ def main():
         choice = input(">>> ").lower()
         if choice == "l":
             filename = input("Filename to load from: ")
-
+            projects = load_projects(filename)
         elif choice == "s":
             filename = input("Filename to save to: ")
-
-        elif choice == "d":
-
-        elif choice == "f":
-
-        elif choice == "a":
-
-        elif choice == "u":
-
+            save_projects(filename, projects)
+        # elif choice == "d":
+        #     display_projects(projects)
+        # elif choice == "f":
+        #     filter_projects(projects)
+        # elif choice == "a":
+        #     add_project(projects)
+        # elif choice == "u":
+        #     update_project(projects)
 
     save_choice = input("Would you like to save to projects.txt? ")
     if save_choice.lower().startswith("y"):
         save_projects("projects.txt", projects)
     print("Thank you for using custom-built project management software.")
 
+
+def load_projects(filename="projects.txt"):
+    """Load projects from a file."""
+    projects = []
+    with open(filename, "r") as in_file:
+        in_file.readline()
+        for line in in_file:
+            parts = line.strip().split("\t")
+            name = parts[0]
+            start_date = datetime.datetime.strptime(parts[1], "%d/%m/%Y").date()
+            priority = int(parts[2])
+            cost_estimate = float(parts[3])
+            completion = int(parts[4])
+            project = Project(name, start_date, priority, cost_estimate, completion)
+            projects.append(project)
+    return projects
+
+
+def save_projects(filename, projects):
+    """Save projects to the file."""
+    with open(filename, "w") as out_file:
+        print("Name\tStart Date\tPriority\tCost Estimate\tCompletion Percentage", file=out_file)
+        for project in projects:
+            print(
+                f"{project.name}\t{project.start_date.strftime('%d/%m/%Y')}\t{project.priority}\t{project.cost_estimate}\t{project.completion_percentage}", file=out_file)
+    print(f"Projects saved to {filename}")
+
+if __name__ == "__main__":
+    main()
