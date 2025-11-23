67326a0ec811928d941a357f848a2336b9b64302
ZhangJiayu0201 <251537931@qq.com>
2025-11-23 21:04:16 +0800

    Improve SilverServiceTaxi test and taxi.py

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -1,19 +1,35 @@
 from silver_service_taxi import SilverServiceTaxi
 
 
+def test_example():
+    """Test example."""
+    drive_distance = 18
+    fanciness = 2
+    taxi = SilverServiceTaxi("Example Taxi", 100, fanciness)
+    flagfall = taxi.flagfall
+    price_per_km = taxi.price_per_km
+    taxi.drive(drive_distance)
+    fare = taxi.get_fare()
+    # assert round(fare, 2) == round(price_per_km * drive_distance + flagfall,2), f"Expected ${price_per_km * drive_distance + flagfall}, got ${fare}"
+    assert round(fare, 1) == round(price_per_km * drive_distance + flagfall,1), f"Expected ${price_per_km * drive_distance + flagfall}, got ${fare}"
+    print(f"For an {drive_distance} km trip in a SilverServiceTaxi with fanciness of {fanciness}, the fare should be {fare}")
+
+
 def test_flagfall():
     """Test for flagfall is always charged."""
-    taxi = SilverServiceTaxi("Test Taxi", 100, 1.0)
+
+    taxi = SilverServiceTaxi("Test flagfall Taxi", 100, 1.0)
     fare = taxi.get_fare()
-    assert fare == 4.50, f"Expected $4.50 flagfall, got ${fare}"
+    assert round(fare, 2) == 4.50, f"Expected $4.50 flagfall, got ${fare}"
 
 
 def test_fanciness():
     """Test fanciness affects."""
-    taxi = SilverServiceTaxi("Test Taxi", 100, 4.0)
+    taxi = SilverServiceTaxi("Test fanciness Taxi", 100, 4.0)
     expected_price = 1.23 * 4.0
-    assert taxi.price_per_km == expected_price, f"Expected ${expected_price}/km, got ${taxi.price_per_km}/km"
+    assert round(taxi.price_per_km, 2) == expected_price, f"Expected ${expected_price}/km, got ${taxi.price_per_km}/km"
 
 
-    test_flagfall()
-    test_fanciness()
+test_example()
+test_flagfall()
+test_fanciness()
